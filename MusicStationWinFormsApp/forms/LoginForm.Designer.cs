28cef9e8a2200f7a9f7ab85cbd4f85fa902c7e79
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-03-29 15:14:48 -0300

    style: melhoria visual tela de login

--- MusicStationWinFormsApp/forms/LoginForm.Designer.cs
+++ MusicStationWinFormsApp/forms/LoginForm.Designer.cs
@@ -35,15 +35,22 @@
             cboLembrarDados = new CheckBox();
             txtUsuario = new TextBox();
             txtSenha = new TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pnlUsuario = new Panel();
+            pnlSenha = new Panel();
             ((System.ComponentModel.ISupportInitialize)pcoLogo).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            pnlUsuario.SuspendLayout();
+            pnlSenha.SuspendLayout();
             SuspendLayout();
             // 
             // lblUsuario
             // 
             lblUsuario.AutoSize = true;
             lblUsuario.Font = new Font("Segoe UI", 10F);
-            lblUsuario.ForeColor = Color.FromArgb(174, 173, 172);
-            lblUsuario.Location = new Point(156, 182);
+            lblUsuario.ForeColor = Color.Gainsboro;
+            lblUsuario.Location = new Point(0, 0);
+            lblUsuario.Margin = new Padding(0);
             lblUsuario.Name = "lblUsuario";
             lblUsuario.Size = new Size(56, 19);
             lblUsuario.TabIndex = 3;
@@ -63,8 +70,9 @@
             // 
             lblSenha.AutoSize = true;
             lblSenha.Font = new Font("Segoe UI", 10F);
-            lblSenha.ForeColor = Color.FromArgb(174, 173, 172);
-            lblSenha.Location = new Point(156, 247);
+            lblSenha.ForeColor = Color.Gainsboro;
+            lblSenha.Location = new Point(0, 72);
+            lblSenha.Margin = new Padding(0);
             lblSenha.Name = "lblSenha";
             lblSenha.Size = new Size(46, 19);
             lblSenha.TabIndex = 8;
@@ -74,11 +82,12 @@
             // 
             btnEntrar.BackColor = Color.FromArgb(95, 97, 231);
             btnEntrar.FlatStyle = FlatStyle.Popup;
-            btnEntrar.Font = new Font("Segoe UI", 10F);
+            btnEntrar.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             btnEntrar.ForeColor = Color.Transparent;
-            btnEntrar.Location = new Point(156, 339);
+            btnEntrar.Location = new Point(3, 176);
+            btnEntrar.Margin = new Padding(3, 5, 3, 3);
             btnEntrar.Name = "btnEntrar";
-            btnEntrar.Size = new Size(463, 28);
+            btnEntrar.Size = new Size(471, 40);
             btnEntrar.TabIndex = 9;
             btnEntrar.Text = "Entrar";
             btnEntrar.UseVisualStyleBackColor = false;
@@ -87,11 +96,13 @@
             // cboLembrarDados
             // 
             cboLembrarDados.AutoSize = true;
+            cboLembrarDados.FlatStyle = FlatStyle.Flat;
             cboLembrarDados.Font = new Font("Segoe UI", 10F);
             cboLembrarDados.ForeColor = Color.Transparent;
-            cboLembrarDados.Location = new Point(156, 310);
+            cboLembrarDados.Location = new Point(3, 140);
             cboLembrarDados.Name = "cboLembrarDados";
-            cboLembrarDados.Size = new Size(104, 23);
+            cboLembrarDados.Padding = new Padding(0, 5, 0, 0);
+            cboLembrarDados.Size = new Size(101, 28);
             cboLembrarDados.TabIndex = 17;
             cboLembrarDados.Text = "Lembrar-me";
             cboLembrarDados.UseVisualStyleBackColor = true;
@@ -99,48 +110,95 @@
             // txtUsuario
             // 
             txtUsuario.BackColor = Color.FromArgb(28, 24, 22);
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Dock = DockStyle.Fill;
             txtUsuario.Font = new Font("Segoe UI", 10F);
-            txtUsuario.ForeColor = SystemColors.ControlDark;
-            txtUsuario.Location = new Point(156, 207);
+            txtUsuario.ForeColor = Color.Gray;
+            txtUsuario.Location = new Point(12, 10);
+            txtUsuario.Margin = new Padding(0);
+            txtUsuario.Multiline = true;
             txtUsuario.Name = "txtUsuario";
             txtUsuario.PlaceholderText = "Digite seu usuario ou email...";
-            txtUsuario.Size = new Size(463, 25);
+            txtUsuario.Size = new Size(447, 20);
             txtUsuario.TabIndex = 18;
-            txtUsuario.Text = "joao";
+            txtUsuario.Enter += txtUsuario_Enter;
             // 
             // txtSenha
             // 
             txtSenha.BackColor = Color.FromArgb(28, 24, 22);
+            txtSenha.BorderStyle = BorderStyle.None;
+            txtSenha.Dock = DockStyle.Fill;
             txtSenha.Font = new Font("Segoe UI", 10F);
-            txtSenha.ForeColor = SystemColors.ControlDark;
-            txtSenha.Location = new Point(156, 269);
+            txtSenha.ForeColor = Color.Gray;
+            txtSenha.Location = new Point(12, 10);
+            txtSenha.Margin = new Padding(0);
+            txtSenha.Multiline = true;
             txtSenha.Name = "txtSenha";
-            txtSenha.PlaceholderText = "DIgite sua senha...";
-            txtSenha.Size = new Size(463, 25);
+            txtSenha.PlaceholderText = "Digite sua senha...";
+            txtSenha.Size = new Size(447, 20);
             txtSenha.TabIndex = 19;
-            txtSenha.Text = "hash123";
+            txtSenha.UseSystemPasswordChar = true;
+            txtSenha.Enter += txtSenha_Enter;
             // 
-            // TelaLogin
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(lblUsuario);
+            flowLayoutPanel1.Controls.Add(pnlUsuario);
+            flowLayoutPanel1.Controls.Add(lblSenha);
+            flowLayoutPanel1.Controls.Add(pnlSenha);
+            flowLayoutPanel1.Controls.Add(cboLembrarDados);
+            flowLayoutPanel1.Controls.Add(btnEntrar);
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(148, 182);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(485, 226);
+            flowLayoutPanel1.TabIndex = 20;
+            // 
+            // pnlUsuario
+            // 
+            pnlUsuario.BackColor = Color.FromArgb(28, 24, 22);
+            pnlUsuario.Controls.Add(txtUsuario);
+            pnlUsuario.Location = new Point(3, 22);
+            pnlUsuario.Margin = new Padding(3, 3, 3, 10);
+            pnlUsuario.Name = "pnlUsuario";
+            pnlUsuario.Padding = new Padding(12, 10, 12, 10);
+            pnlUsuario.Size = new Size(471, 40);
+            pnlUsuario.TabIndex = 21;
+            pnlUsuario.Paint += pnlUsuario_Paint;
+            // 
+            // pnlSenha
+            // 
+            pnlSenha.BackColor = Color.FromArgb(28, 24, 22);
+            pnlSenha.Controls.Add(txtSenha);
+            pnlSenha.Location = new Point(3, 94);
+            pnlSenha.Name = "pnlSenha";
+            pnlSenha.Padding = new Padding(12, 10, 12, 10);
+            pnlSenha.Size = new Size(471, 40);
+            pnlSenha.TabIndex = 22;
+            pnlSenha.Paint += pnlSenha_Paint;
+            // 
+            // LoginForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(28, 24, 22);
             ClientSize = new Size(800, 450);
-            Controls.Add(txtSenha);
-            Controls.Add(txtUsuario);
-            Controls.Add(cboLembrarDados);
-            Controls.Add(btnEntrar);
-            Controls.Add(lblSenha);
             Controls.Add(pcoLogo);
-            Controls.Add(lblUsuario);
+            Controls.Add(flowLayoutPanel1);
             ForeColor = SystemColors.ControlText;
             FormBorderStyle = FormBorderStyle.None;
-            Name = "TelaLogin";
+            Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tela Login";
+            Load += LoginForm_Load;
             ((System.ComponentModel.ISupportInitialize)pcoLogo).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            pnlUsuario.ResumeLayout(false);
+            pnlUsuario.PerformLayout();
+            pnlSenha.ResumeLayout(false);
+            pnlSenha.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -151,5 +209,8 @@
         private CheckBox cboLembrarDados;
         private TextBox txtUsuario;
         private TextBox txtSenha;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel pnlSenha;
+        private Panel pnlUsuario;
     }
 }
--- MusicStationWinFormsApp/forms/LoginForm.cs
+++ MusicStationWinFormsApp/forms/LoginForm.cs
@@ -1,17 +1,18 @@
 using MusicStationWinFormsApp.forms;
+using System.Drawing.Drawing2D;
 
 namespace MusicStationWinFormsApp
 {
-    public partial class LoginForm : Form 
+    public partial class LoginForm : Form
     {
         public LoginForm()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-
             if (string.IsNullOrWhiteSpace(txtUsuario.Text) ||
                 string.IsNullOrWhiteSpace(txtSenha.Text))
             {
@@ -29,8 +30,50 @@ namespace MusicStationWinFormsApp
             }
             else
             {
-                MessageBox.Show("Usuário ou senhas inválidos");
+                MessageBox.Show("Usuário ou senhas inválidos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
+        }
+
+        // Configurando borda personalizada no pnlUsuario e pnlSenha
+        public void CustomizarPainel(object sender, PaintEventArgs e)
+        {
+            Control paintControl = (Control)sender;
+            Color corBorda = Color.FromArgb(95, 97, 231);
+            int espessura = 2; // Espessura da borda
+
+            // Desenha a borda manual
+            using (Pen pen = new Pen(corBorda, espessura))
+            {
+                // Ajuste para a borda não ser cortada
+                e.Graphics.DrawRectangle(pen, 0, 0, paintControl.Width - 1, paintControl.Height - 1);
+            }
+        }
+
+        private void pnlUsuario_Paint(object sender, PaintEventArgs e)
+        {
+            CustomizarPainel(sender, e);
+        }
+
+        private void pnlSenha_Paint(object sender, PaintEventArgs e)
+        {
+            CustomizarPainel(sender, e);
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            txtUsuario.PlaceholderText = null;
+            txtUsuario.Text = "joao";
+        }
+
+        private void txtSenha_Enter(object sender, EventArgs e)
+        {
+            txtSenha.PlaceholderText = null;
+            txtSenha.Text = "hash123";
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
