ebced747dfee00c7ff754f3c774c8b9868a3a133
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-03-29 15:18:30 -0300

    refactor: atualização dos campos da entidade usuário

--- MusicStationWinFormsApp/models/Usuario.cs
+++ MusicStationWinFormsApp/models/Usuario.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace MusicStationWinFormsApp.models
 {
-    internal class Usuario
+    public class Usuario
     {
-        public int id_usuario { get; set; }
-        public string nome { get; set; }
-        public string email { get; set; }
-        public string usuario_nome { get; set; }
-        public string senha { get; set; }
-        public DateTime data_cadastro { get; set; }
+        public int IdUsuario { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+        public string UsuarioNome { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
     }
 }
