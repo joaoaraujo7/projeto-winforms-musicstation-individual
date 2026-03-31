07bb9739da316a7193243590a4cc1b6e5038699a
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-03-31 10:45:38 -0300

    refactor: ajustando tela de login como tela inicial e imagem do dashboard

--- MusicStationWinFormsApp/controls/dashboard/DashboardControl.Designer.cs
+++ MusicStationWinFormsApp/controls/dashboard/DashboardControl.Designer.cs
@@ -28,90 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            panel4 = new Panel();
-            panel5 = new Panel();
-            panel6 = new Panel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // panel1
+            // pictureBox1
             // 
-            panel1.BackColor = Color.Gray;
-            panel1.ForeColor = SystemColors.ControlText;
-            panel1.Location = new Point(115, 51);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(200, 100);
-            panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.Gray;
-            panel2.ForeColor = SystemColors.ControlText;
-            panel2.Location = new Point(433, 51);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(200, 100);
-            panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = Color.Gray;
-            panel3.ForeColor = SystemColors.ControlText;
-            panel3.Location = new Point(115, 196);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(200, 100);
-            panel3.TabIndex = 1;
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.Gray;
-            panel4.ForeColor = SystemColors.ControlText;
-            panel4.Location = new Point(433, 338);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(200, 100);
-            panel4.TabIndex = 1;
-            // 
-            // panel5
-            // 
-            panel5.BackColor = Color.Gray;
-            panel5.ForeColor = SystemColors.ControlText;
-            panel5.Location = new Point(115, 338);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(200, 100);
-            panel5.TabIndex = 2;
-            // 
-            // panel6
-            // 
-            panel6.BackColor = Color.Gray;
-            panel6.ForeColor = SystemColors.ControlText;
-            panel6.Location = new Point(433, 196);
-            panel6.Name = "panel6";
-            panel6.Size = new Size(200, 100);
-            panel6.TabIndex = 1;
+            pictureBox1.Image = Properties.Resources.dashboard1;
+            pictureBox1.Location = new Point(33, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(872, 442);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
             // 
             // DashboardControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(panel6);
-            Controls.Add(panel5);
-            Controls.Add(panel4);
-            Controls.Add(panel3);
-            Controls.Add(panel2);
-            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
             Name = "DashboardControl";
             Size = new Size(744, 511);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
-        private Panel panel2;
-        private Panel panel3;
-        private Panel panel4;
-        private Panel panel5;
-        private Panel panel6;
+        private PictureBox pictureBox1;
     }
 }
