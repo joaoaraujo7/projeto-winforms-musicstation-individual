1115aed7d27053cde27f6f5a262693aabe986abd
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-05-07 17:19:10 -0300

    feat(admin): adicionar método ObterTodos - Admin

--- MusicStationWinFormsApp/Repository/AdministradorRepository.cs
+++ MusicStationWinFormsApp/Repository/AdministradorRepository.cs
@@ -1,15 +1,26 @@
 ﻿using MusicStationWinFormsApp.Data;
 using MusicStationWinFormsApp.Models;
+using MusicStationWinFormsApp.ViewModel;
 
 namespace MusicStationWinFormsApp.repository
 {
     internal class AdministradorRepository
     {
-        public List<Administrador> ListarTodos()
+        public List<AdministradorGridViewModel> ObterTodos()
         {
             using (var db = new MusicStationDbContext())
             {
-                return db.Administradores.ToList();
+                return db.Administradores.Select(a => new AdministradorGridViewModel
+                {
+                    IdAdmin = a.IdAdmin,
+                    Nome = a.Usuario.Nome,
+                    Email = a.Usuario.Email,
+                    UsuarioNome = a.Usuario.UsuarioNome,
+                    DataCadastro = a.Usuario.DataCadastro,
+                    NivelAcesso = a.NivelAcesso,
+                    Observacoes = a.Observacoes
+                })
+                .ToList();
             }
         }
     }
