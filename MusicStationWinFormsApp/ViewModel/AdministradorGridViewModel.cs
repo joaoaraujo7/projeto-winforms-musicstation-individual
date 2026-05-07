c83f777b22265bfe16c09a71b83114cf733b61fe
joaoaraujo7 <jpedro.araujo21@gmail.com>
2026-05-07 17:17:54 -0300

    feat(admin): criar AdministradorGridViewModel

---
+++ MusicStationWinFormsApp/ViewModel/AdministradorGridViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStationWinFormsApp.ViewModel
+{
+    public class AdministradorGridViewModel
+    {
+        public int IdAdmin { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string UsuarioNome { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int NivelAcesso { get; set; }
+        public string? Observacoes { get; set; }
+    }
+}
